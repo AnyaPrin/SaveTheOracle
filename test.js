2b2c09d2e72d4db99b3712d3642e2e9807594b30
Anya Prin <146047541+AnyaPrin@users.noreply.github.com>
2025-07-17 20:46:34 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,21 +1,18 @@
-// test.js
 document.addEventListener("DOMContentLoaded", function () {
-    // HTMLの読み込みが完了したら実行
-    var canvas = document.getElementById('rectangle');
+    var canvas = document.getElementById('gameCanvas');
     if ( ! canvas || ! canvas.getContext ) {
         return false;
     }
-    const canvas = document.getElementById('gameCanvas');
     const ctx = canvas.getContext('2d');
     ctx.font = '24px Arial';
     ctx.fillText('Puzzle Game (JS)', 25, 100);
-    ctx.beginPath(); /* 図形を描き始めることを宣言 */
-    ctx.moveTo(50, 50); /* 図形の描き始めを移動 */
-    ctx.lineTo(150, 50); /* 図形の線の終わりを決める */
+    ctx.beginPath(); 
+    ctx.moveTo(50, 50);
+    ctx.lineTo(150, 50);
     ctx.lineTo(150, 150);
     ctx.lineTo(50, 150);
-    ctx.closePath(); /* 描いた線を閉じる */
-    ctx.stroke(); /* 描いた図形を線で表示させる */
+    ctx.closePath();
+    ctx.stroke();
     const outputDiv = document.getElementById("output");
     if (outputDiv) {
         outputDiv.innerHTML = "<p>Hello, World! This is a test from test.js</p>";
