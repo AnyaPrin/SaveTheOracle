0795d83e4b13f57c8a741f3a9145a0a4ea322c08
Anya Prin <146047541+AnyaPrin@users.noreply.github.com>
2025-07-18 01:08:37 +0900

    Update minfilia.js

--- minfilia.js
+++ minfilia.js
@@ -430,7 +430,7 @@ function mainLoop() {
 }
 
 // 初期化
-window.onload = async function() {
+/* window.onload = async function() {
     canvas = document.getElementById("gamecanvas");
     ctx = canvas.getContext("2d");
     canvas.width = SCRN_W;
@@ -442,5 +442,26 @@ window.onload = async function() {
     canvas.addEventListener("mouseup", onMouseUp);
     mainLoop();
 };
+*/
 
+document.addEventListener("DOMContentLoaded", function () {
+    const canvas = document.getElementById('gameCanvas');
+    if ( ! canvas || ! canvas.getContext ) {return false;}
+    const ctx = canvas.getContext('2d');
+    ctx.font = '24px Arial';
+    ctx.fillText('Puzzle Game (JS)', 25, 100);
+    ctx.beginPath();
+    ctx.moveTo(50, 50);
+    ctx.lineTo(150, 50);
+    ctx.lineTo(150, 150);
+    ctx.lineTo(50, 150);
+    ctx.closePath();
+    ctx.stroke();
+    const outputDiv = document.getElementById("output");
+    if (outputDiv) {
+        outputDiv.innerHTML = "<p>Hello, World! This is a test from test.js</p>";
+    } else {
+        console.error("Output div not found!");
+    }
+});
 
